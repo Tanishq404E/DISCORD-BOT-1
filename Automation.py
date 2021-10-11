a0a257b9af0cd73e4f3cf2972a67b9d64eca4ece
Tanishq Tanwar <tanishqtanwar1976@gmail.com>
2021-10-11 20:27:04 +0530

    Discord bot code (provides meet link)

---
+++ Automation.py
@@ -0,0 +1,20 @@
+import os
+import discord
+# Replace "TOKEN" with your discord bot token
+my_secret="TOKEN"
+client = discord.Client()
+@client.event
+async def on_read():
+  print("Bot logged in as{0.user}".format(client))
+@client.event
+async def on_message(msg):
+  if msg.author==client.user:
+    return
+  if msg.content.startswith('!link'):
+    await msg.channel.send("https://meet.google.com/fxk-vdiv-tcu")#on asking for link it will send a valid pre-stored link to the meet
+  elif msg.content.startswith('!bye'):
+    await msg.channel.send("Bye ,have a nice day")
+  elif msg.content.startswith('!goodnight'):
+    await msg.channel.send("goodnight, sweet dreams")
+client.run(my_secret)
+
